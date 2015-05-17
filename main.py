04f0cf439292267ca2b53972cb3662c13f22a88e
Jeff Tsai <jefftsai8049@gmail.com>
2015-05-17 15:16:07 +0800

    add load map file

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import readNovel
 
 novel = readNovel.readNovel()
 
-# print("Read and Split Data : " + str(novel.readSplit("train_data/training.txt","train_data/training_split.txt", "train_data/useless.txt")))
-# print("Preprocessing Data :　" + str(novel.preprocessing("train_data/training_split.txt", "train_data/training_out.csv")))
+print("Read and Split Data : " + str(novel.readSplit("train_data/training.txt","train_data/training_split.txt", "train_data/useless.txt")))
+print("Preprocessing Data :　" + str(novel.preprocessing("train_data/training_split.txt", "train_data/training_out.csv")))
 
 print(novel.word2index("train_data/training_out.csv","train_data/training_index.csv","train_data/train_map.csv"))
+
+# print(novel.loadWord2VectorMap("train_data/word2vec/training_classes_500.txt"))
