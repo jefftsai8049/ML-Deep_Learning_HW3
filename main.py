6d84e7f9464ca4440b000ac65873af097d0d2cb4
Jeff Tsai <jefftsai8049@gmail.com>
2015-05-17 22:45:25 +0800

    command and train data word2vec index ok

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ __author__ = 'jefftsai'
 import readNovel
 
 novel = readNovel.readNovel()
-
+# for read raw data and remove useless word
 # print("Read and Split Data : " + str(novel.readSplit("train_data/training.txt","train_data/training_split.txt", "train_data/useless.txt")))
+
+# convert to lower and remove empty line
 # print("Preprocessing Data :　" + str(novel.preprocessing("train_data/training_split.txt", "train_data/training_out.csv")))
 
+# convert to index (current no use)
 # print(novel.word2index("train_data/training_out.csv","train_data/training_index.csv","train_data/train_map.csv"))
 
+# convert to index(by google code word2vec)
 print(novel.word2VectorIndex("train_data/word2vec/word2vec_map_100.txt","train_data/training_out.csv","train_data/training_index_word2vec100.csv"))
